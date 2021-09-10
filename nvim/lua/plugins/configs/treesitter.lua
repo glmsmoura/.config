22920e844fb15d3c10615544c17ae0c32edfd2f1
Gabriel Moura <rigbydead@gmail.com>
2021-09-10 19:39:10 -0300

    Add nvchad

---
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,19 @@
+local present, ts_config = pcall(require, "nvim-treesitter.configs")
+if not present then
+   return
+end
+
+ts_config.setup {
+   ensure_installed = {
+      "lua",
+   },
+   highlight = {
+      enable = true,
+      use_languagetree = true,
+   },
+   rainbow = {
+     enable = true,
+     extended_mode = true,
+     max_file_lines = 1000,
+ },
+}
